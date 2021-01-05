6e382342338ec7fc46a0210382eb86c74b4f2cf3
Krzysiek Kostkiewicz <krzysztof.kostkiewicz@gmail.com>
2021-01-05 13:49:50 +0100

    added invoices model

---
+++ fakturogen-invoices/src/test/java/pl/fakturogen/fakturogeninvoices/FakturogenInvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fakturogen.fakturogeninvoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturogenInvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
