16e90667ae83274965e4bccc6ca2a792ba96de5a
ewa-git <agnieszka.sieminska@gmail.com>
2021-01-12 20:43:36 +0100

    Entities/ added customer entity

--- fakturogen-invoices/src/main/java/pl/fakturogen/fakturogeninvoices/model/Address.java
+++ fakturogen-invoices/src/main/java/pl/fakturogen/fakturogeninvoices/model/Address.java
@@ -1,6 +1,7 @@
 package pl.fakturogen.fakturogeninvoices.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = Address.TABLE_NAME)
@@ -25,4 +26,77 @@ public class Address {
 
     @Column(name = "city", nullable = false)
     private String city;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return id == address.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", buildingNumber='" + buildingNumber + '\'' +
+                ", flatNumber='" + flatNumber + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
--- fakturogen-invoices/src/main/java/pl/fakturogen/fakturogeninvoices/model/Customer.java
+++ fakturogen-invoices/src/main/java/pl/fakturogen/fakturogeninvoices/model/Customer.java
@@ -1,6 +1,8 @@
 package pl.fakturogen.fakturogeninvoices.model;
 
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = Customer.TABLE_NAME)
@@ -20,19 +22,109 @@ public class Customer {
     @Column(name = "customer_code", nullable = false)
     private String customerCode;
 
-    @Column(name = "mail", nullable = false)
+    @Column(name = "mail")
     private String mail;
 
     @Column(name = "phone_number")
     private String phoneNumber;
 
     @Enumerated
+    @Column(name = "customer_type", nullable = false)
     private CustomerType customerType;
 
     @OneToOne
     private Address address;
 
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerTaxNumber() {
+        return customerTaxNumber;
+    }
+
+    public void setCustomerTaxNumber(String customerTaxNumber) {
+        this.customerTaxNumber = customerTaxNumber;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id &&
+                Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", customerTaxNumber='" + customerTaxNumber + '\'' +
+                ", customerCode='" + customerCode + '\'' +
+                ", mail='" + mail + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", customerType=" + customerType +
+                ", address=" + address +
+                '}';
+    }
 }
