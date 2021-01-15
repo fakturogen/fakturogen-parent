2854f67f171b1b2550a36ab24264ec24c18c508d
Damian Rowiński <damian.rowinski@gmail.com>
2021-01-15 22:00:19 +0100

    added lombok and Java 11 version

--- fakturogen-invoice/src/main/java/pl/fakturogen/invoice/web/dto/ProductDTO.java
+++ fakturogen-invoice/src/main/java/pl/fakturogen/invoice/web/dto/ProductDTO.java
@@ -1,4 +1,16 @@
 package pl.fakturogen.invoice.web.dto;
 
+import pl.fakturogen.invoice.dao.entity.Rate;
+
+@Data
 public class ProductDTO {
+
+    private String name;
+    private String description;
+    private String itemCode;
+    private String unitOfMeasurement;
+    private double saleNetPrice;
+    private double saleGrossPrice;
+    private Rate rate;
+    private long idExternalApi;
 }
