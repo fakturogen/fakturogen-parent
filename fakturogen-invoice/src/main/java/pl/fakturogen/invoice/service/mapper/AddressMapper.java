c31b86195ed660a28d9cf096e9620ba513de24e6
ewa-git <agnieszka.sieminska@gmail.com>
2021-01-23 15:53:16 +0100

    added mapper for address

---
+++ fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/mapper/AddressMapper.java
@@ -0,0 +1,34 @@
+package pl.fakturogen.invoice.service.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+import pl.fakturogen.invoice.dao.entity.Address;
+import pl.fakturogen.invoice.web.dto.AddressDTO;
+
+@Component
+@Slf4j
+public class AddressMapper {
+
+    private ModelMapper modelMapper;
+
+    public AddressMapper(){
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    public AddressDTO from(Address address){
+        log.info("mapping from {}", address);
+        AddressDTO addressDTO = modelMapper.map(address, AddressDTO.class);
+        log.info("mapping from {} = {}", address, addressDTO);
+        return addressDTO;
+    }
+
+    public Address from(AddressDTO addressDTO){
+        log.info("mapping from {}", addressDTO);
+        Address address = modelMapper.map(addressDTO, Address.class);
+        log.info("mapping from {} = {}", addressDTO, address);
+        return address;
+    }
+}
