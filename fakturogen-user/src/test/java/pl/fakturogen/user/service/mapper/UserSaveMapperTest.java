b3747c80a2166c8dfe62feec256a85926846625b
Krzysiek Kostkiewicz <krzysztof.kostkiewicz@gmail.com>
2021-01-27 14:08:56 +0100

    Added fields to User

---
+++ fakturogen-user/src/test/java/pl/fakturogen/user/service/mapper/UserSaveMapperTest.java
@@ -0,0 +1,56 @@
+package pl.fakturogen.user.service.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pl.fakturogen.user.dao.entity.User;
+import pl.fakturogen.user.web.dto.UserSaveDTO;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserSaveMapperTest {
+    UserSaveMapper userSaveMapper;
+    UserSaveDTO userSaveDTO;
+    User user;
+
+    @BeforeEach
+    void setup() {
+        userSaveMapper = new UserSaveMapper();
+    }
+
+    @DisplayName(" - should return User filled with all fields")
+    @Test
+    public void test1() {
+        userSaveDTO = UserSaveDTO.builder()
+                .id(1L)
+                .email("test@email.com")
+                .firstname("Jan")
+                .lastname("Kowalski")
+                .password("passwd")
+                .rePassword("passwd")
+                .build();
+
+        user = new User();
+        user.setId(userSaveDTO.getId());
+        user.setEmail(userSaveDTO.getEmail());
+        user.setFirstName(userSaveDTO.getFirstname());
+        user.setLastName(userSaveDTO.getLastname());
+        user.setPassword(userSaveDTO.getPassword());
+
+        User result = userSaveMapper.from(userSaveDTO);
+
+        assertEquals(user, result);
+    }
+
+    @DisplayName(" - should return empty object")
+    @Test
+    public void test2() {
+        userSaveDTO = new UserSaveDTO();
+        user = new User();
+        //when
+        User result = userSaveMapper.from(userSaveDTO);
+        //then
+        assertEquals(user, result);
+    }
+
+}
