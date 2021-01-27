c870be3db3bfc6f88536502d21ce51cf354ae03e
Krzysiek Kostkiewicz <krzysztof.kostkiewicz@gmail.com>
2021-01-27 14:10:28 +0100

    Prepared dtos and mapper

--- fakturogen-user/src/main/java/pl/fakturogen/user/web/dto/UserSaveDTO.java
+++ fakturogen-user/src/main/java/pl/fakturogen/user/web/dto/UserSaveDTO.java
@@ -1,13 +1,21 @@
 package pl.fakturogen.user.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-public class UserDTO {
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSaveDTO {
+
     private Long id;
     private String email;
+    private String password;
+    private String rePassword;
     private String firstname;
     private String lastname;
+
 }
