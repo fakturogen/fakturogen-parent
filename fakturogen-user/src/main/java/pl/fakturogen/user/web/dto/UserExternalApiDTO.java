e981983a4dbed7a92164d6e8cc00dc602b3b43cf
ewa-git <agnieszka.sieminska@gmail.com>
2021-01-30 11:57:08 +0100

    added UserExternalApiDTO and UserExternalApiMapper

--- fakturogen-user/src/main/java/pl/fakturogen/user/web/dto/UserExternalApiDTO.java
+++ fakturogen-user/src/main/java/pl/fakturogen/user/web/dto/UserExternalApiDTO.java
@@ -1,4 +1,18 @@
 package pl.fakturogen.user.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserExternalApiDTO {
+
+    private Long id;
+    private String clientId;
+    private String clientSecret;
+
 }
