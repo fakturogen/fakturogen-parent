5a95e13f524369a9fc1945ddfc9faccbb0edddf5
KrzKos <krzysztof.kostkiewicz@gmail.com>
2021-03-03 13:40:56 +0100

    added HttpConnectorUtils (created by Ewa)

--- fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/RestInvoiceController.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/RestInvoiceController.java
@@ -1,13 +1,37 @@
 package pl.fakturogen.web.controller.rest;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.fakturogen.comarch.connector.connectors.ComarchApiInvoiceConnector;
+
+import java.io.IOException;
 
 @RestController
 public class RestInvoiceController {
 
-    /*private TestRepository testRepository;
+    private ApplicationArguments applicationArguments;
+    private ComarchApiInvoiceConnector apiInvoiceConnector;
 
-    public RestInvoiceController(TestRepository testRepository) {
-        this.testRepository = testRepository;
-    }*/
+    public RestInvoiceController(ApplicationArguments applicationArguments, ComarchApiInvoiceConnector apiInvoiceConnector) {
+        this.applicationArguments = applicationArguments;
+        this.apiInvoiceConnector = apiInvoiceConnector;
+    }
+
+    @RequestMapping("/getInvoices")
+    public String getInvoices() throws IOException {
+        String[] args = applicationArguments.getSourceArgs();
+        apiInvoiceConnector.setClientId(args[0]);
+        apiInvoiceConnector.setSecret(args[1]);
+        return apiInvoiceConnector.getInvoices();
+    }
+
+    @RequestMapping("/getInvoices/{id}")
+    public String getInvoice(@PathVariable long id) throws IOException {
+        String[] args = applicationArguments.getSourceArgs();
+        apiInvoiceConnector.setClientId(args[0]);
+        apiInvoiceConnector.setSecret(args[1]);
+        return apiInvoiceConnector.getInvoiceById(id);
+    }
 }
