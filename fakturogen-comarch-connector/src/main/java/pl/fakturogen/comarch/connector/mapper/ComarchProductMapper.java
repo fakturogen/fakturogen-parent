8b31a2fd75b2ea0dc643559290c67c8bc4e2856a
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-05 19:22:04 +0100

    added comarch product mapper

--- fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/mapper/ComarchProductMapper.java
+++ fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/mapper/ComarchProductMapper.java
@@ -1,16 +1,29 @@
 package pl.fakturogen.comarch.connector.mapper;
 
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import pl.fakturogen.comarch.connector.dto.ComarchProductDTO;
 import pl.fakturogen.comarch.connector.model.ComarchProduct;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class ComarchProductMapper {
     public ComarchProductDTO from(ComarchProduct comarchProduct) {
+        log.info("from {{}}", comarchProduct);
+        ModelMapper mapper = new ModelMapper();
+
+        ComarchProductDTO productDTO = mapper.map(comarchProduct, ComarchProductDTO.class);
+        log.info("to {{}}", productDTO);
+        return productDTO;
     }
 
-    public List<ComarchProductDTO> fromList(List<ComarchProduct> comarchProduct) {
+    public List<ComarchProductDTO> fromList(List<ComarchProduct> comarchProductList) {
+        return comarchProductList.stream()
+                .map(this::from)
+                .collect(Collectors.toList());
     }
 }
