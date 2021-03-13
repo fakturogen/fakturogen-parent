d3f0d2a66e9d0ac1dff5312f5b10eab326e1fd6d
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-13 17:58:25 +0100

    updated classes names

--- fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchCustomerService.java
+++ fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchCustomerService.java
@@ -1,4 +1,4 @@
 package pl.fakturogen.comarch.connector.services;
 
-public interface ProductService {
+public interface ComarchCustomerService {
 }
--- fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchProductService.java
+++ fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchProductService.java
@@ -1,4 +1,4 @@
 package pl.fakturogen.comarch.connector.services;
 
-public interface CustomerService {
+public interface ComarchProductService {
 }
