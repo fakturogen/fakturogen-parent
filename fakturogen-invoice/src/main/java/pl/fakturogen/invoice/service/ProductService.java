04412cf9bddef6be9d52408b544f696de6952540
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-13 18:49:04 +0100

    created save method in ProductService

--- fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/ProductService.java
+++ fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/ProductService.java
@@ -1,6 +1,18 @@
 package pl.fakturogen.invoice.service;
 
+import org.springframework.stereotype.Service;
+import pl.fakturogen.invoice.web.dto.ProductDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface ProductService {
 
+    ProductDTO create(ProductDTO productDTO);
+    Optional<ProductDTO> read(long id);
+    List<ProductDTO> readAll();
+    void update(ProductDTO productDTO, long id);
+    void delete(ProductDTO productDTO, long id);
 
 }
--- fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/impl/ProductServiceDefault.java
+++ fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/impl/ProductServiceDefault.java
@@ -1,4 +1,49 @@
 package pl.fakturogen.invoice.service.impl;
 
-public class ProductServiceDefault {
+import org.springframework.stereotype.Service;
+import pl.fakturogen.invoice.dao.entity.Product;
+import pl.fakturogen.invoice.dao.repository.ProductRepository;
+import pl.fakturogen.invoice.service.ProductService;
+import pl.fakturogen.invoice.service.mapper.ProductMapper;
+import pl.fakturogen.invoice.web.dto.ProductDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceDefault implements ProductService {
+
+    private ProductRepository productRepository;
+    private ProductMapper productMapper;
+
+    public ProductServiceDefault(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductDTO create(ProductDTO productDTO) {
+        Product product = productMapper.from(productDTO);
+        Product savedProduct = productRepository.save(product);
+        return productMapper.from(savedProduct);
+    }
+
+    @Override
+    public Optional<ProductDTO> read(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<ProductDTO> readAll() {
+        return null;
+    }
+
+    @Override
+    public void update(ProductDTO productDTO, long id) {
+
+    }
+
+    @Override
+    public void delete(ProductDTO productDTO, long id) {
+
+    }
 }
