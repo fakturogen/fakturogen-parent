3b0791e9ac54d950e54566f178dd307cd671c027
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-13 19:41:30 +0100

    added creating Product in database

--- fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/ProductController.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/ProductController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.fakturogen.comarch.connector.dto.ComarchProductDTO;
+import pl.fakturogen.comarch.connector.mapper.FakturogenProductMapper;
 import pl.fakturogen.comarch.connector.services.ComarchProductService;
+import pl.fakturogen.invoice.service.ProductService;
+import pl.fakturogen.invoice.web.dto.ProductDTO;
 import pl.fakturogen.web.exception.ProductNotFoundException;
 
 import java.util.List;
@@ -13,9 +16,14 @@ import java.util.Optional;
 @RestController
 public class ProductController {
     private ComarchProductService comarchProductService;
+    private ProductService productService;
+    private FakturogenProductMapper fakturogenProductMapper;
 
-    public ProductController(ComarchProductService comarchProductService) {
+    public ProductController(ComarchProductService comarchProductService, ProductService productService,
+                             FakturogenProductMapper fakturogenProductMapper) {
         this.comarchProductService = comarchProductService;
+        this.productService = productService;
+        this.fakturogenProductMapper = fakturogenProductMapper;
     }
 
 
@@ -27,6 +35,11 @@ public class ProductController {
     @GetMapping("/getProductById/{id}")
     public ComarchProductDTO getProductById(@PathVariable Long id) throws ProductNotFoundException {
         Optional<ComarchProductDTO> optionalProduct = comarchProductService.read(id);
-        return optionalProduct.orElseThrow(() -> new ProductNotFoundException("Product with given id not found."));
+        ComarchProductDTO comarchProductDTO = optionalProduct
+                .orElseThrow(() -> new ProductNotFoundException("Product with given id not found."));
+
+        ProductDTO productDTO = fakturogenProductMapper.from(comarchProductDTO);
+        productService.create(productDTO);
+        return comarchProductDTO;
     }
 }
