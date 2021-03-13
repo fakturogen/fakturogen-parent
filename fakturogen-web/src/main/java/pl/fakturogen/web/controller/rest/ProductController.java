b8ab4fd4a97817a0562ee24fc9291d1bc838b36a
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-13 19:39:12 +0100

    changed connector to service

--- fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/ProductController.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/ProductController.java
@@ -3,8 +3,8 @@ package pl.fakturogen.web.controller.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import pl.fakturogen.comarch.connector.connector.ComarchApiProductConnector;
 import pl.fakturogen.comarch.connector.dto.ComarchProductDTO;
+import pl.fakturogen.comarch.connector.services.ComarchProductService;
 import pl.fakturogen.web.exception.ProductNotFoundException;
 
 import java.util.List;
@@ -12,20 +12,21 @@ import java.util.Optional;
 
 @RestController
 public class ProductController {
-    private ComarchApiProductConnector comarchApiProductConnector;
+    private ComarchProductService comarchProductService;
 
-    public ProductController(ComarchApiProductConnector comarchApiProductConnector) {
-        this.comarchApiProductConnector = comarchApiProductConnector;
+    public ProductController(ComarchProductService comarchProductService) {
+        this.comarchProductService = comarchProductService;
     }
+
 
     @GetMapping("/getProductList")
     public List<ComarchProductDTO> getProductList() {
-        return comarchApiProductConnector.readAll();
+        return comarchProductService.readAll();
     }
 
     @GetMapping("/getProductById/{id}")
     public ComarchProductDTO getProductById(@PathVariable Long id) throws ProductNotFoundException {
-        Optional<ComarchProductDTO> optionalProduct = comarchApiProductConnector.read(id);
+        Optional<ComarchProductDTO> optionalProduct = comarchProductService.read(id);
         return optionalProduct.orElseThrow(() -> new ProductNotFoundException("Product with given id not found."));
     }
 }
