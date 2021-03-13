2c266104cd483e308471195094f07560c1d8deaf
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-13 18:05:25 +0100

    created interface

--- fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchProductService.java
+++ fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchProductService.java
@@ -1,4 +1,13 @@
 package pl.fakturogen.comarch.connector.services;
 
+import pl.fakturogen.comarch.connector.dto.ComarchProductDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ComarchProductService {
+
+    Optional<ComarchProductDTO> read(Long id);
+    List<ComarchProductDTO> readAll();
+
 }
