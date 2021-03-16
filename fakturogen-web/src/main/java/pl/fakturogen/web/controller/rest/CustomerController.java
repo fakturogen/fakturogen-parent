7be26e822caef137913d04a2c59747d743d7d0b3
ewa-git <agnieszka.sieminska@gmail.com>
2021-03-16 21:48:39 +0100

    Fixed post method in CustomerController

--- fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/CustomerController.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.fakturogen.comarch.connector.connector.ComarchApiCustomerConnector;
+import pl.fakturogen.comarch.connector.dto.ComarchAddressDTO;
 import pl.fakturogen.comarch.connector.dto.ComarchCustomerDTO;
 
 import java.io.IOException;
@@ -30,15 +31,15 @@ public class CustomerController {
     @GetMapping("/{id}")
     public ComarchCustomerDTO read(@PathVariable long id) throws Exception {
         Optional<ComarchCustomerDTO> optionalCustomer = comarchApiCustomerConnector.read(id);
-        //ComarchCustomerDTO comarchCustomerDTO = optionalCustomer.orElseThrow(() -> new Exception("Customer with id " + id + " was not found"));
-        // dorobic wyjatek  i zwrocic or else throw
-        ComarchCustomerDTO comarchCustomerDTO = optionalCustomer.get();
+        ComarchCustomerDTO comarchCustomerDTO = optionalCustomer.orElseThrow(() -> new Exception("Customer with id " + id + " was not found"));
+//        ComarchCustomerDTO comarchCustomerDTO = optionalCustomer.get();
         return comarchCustomerDTO;
     }
 
     @PostMapping
     public Long create() throws IOException {
         ComarchCustomerDTO customerComarchDTO = new ComarchCustomerDTO();
+        ComarchAddressDTO comarchAddressDTO = new ComarchAddressDTO();
         customerComarchDTO.setName("NAME");
         Random random = new Random();
         customerComarchDTO.setCustomerTaxNumber("K/001");
@@ -46,6 +47,7 @@ public class CustomerController {
         customerComarchDTO.setCustomerCode(String.valueOf(random.nextInt(100)));
         customerComarchDTO.setMail("MAIL");
         customerComarchDTO.setPhoneNumber("PHONE_NUMBER");
+        customerComarchDTO.setAddress(comarchAddressDTO);
 
         Long externalId = comarchApiCustomerConnector.create(customerComarchDTO);
         return externalId;
