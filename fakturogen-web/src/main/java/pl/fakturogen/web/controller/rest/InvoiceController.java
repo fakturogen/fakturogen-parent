eb21d46ee95b73d45b607c9bbe88d61df9a7a11f
KrzKos <krzysztof.kostkiewicz@gmail.com>
2021-03-19 11:37:10 +0100

    added readAll method

--- fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/InvoiceController.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/controller/rest/InvoiceController.java
@@ -6,19 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.fakturogen.comarch.connector.dto.ComarchInvoiceDTO;
 import pl.fakturogen.comarch.connector.services.ComarchInvoiceService;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
-public class RestInvoiceController {
+public class InvoiceController {
 
     private ComarchInvoiceService comarchInvoiceService;
 
-    public RestInvoiceController(ComarchInvoiceService comarchInvoiceService) {
+    public InvoiceController(ComarchInvoiceService comarchInvoiceService) {
         this.comarchInvoiceService = comarchInvoiceService;
     }
 
-/*    @GetMapping("/getInvoices")
-    public List<ComarchInvoice> getInvoices() throws IOException {
-        return comarchApiInvoiceConnector.getInvoices();
-    }*/
+    @GetMapping("/getInvoices")
+    public List<ComarchInvoiceDTO> getInvoices() throws IOException {
+        return comarchInvoiceService.readAll();
+    }
 
     @GetMapping("/getInvoices/{id}")
     public ComarchInvoiceDTO getInvoice(@PathVariable long id) throws Exception {
