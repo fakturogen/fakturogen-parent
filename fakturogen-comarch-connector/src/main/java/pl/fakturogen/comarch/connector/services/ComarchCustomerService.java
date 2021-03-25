f27f2f19375ec4ce80ecde614f357d74f787c7ad
KrzKos <krzysztof.kostkiewicz@gmail.com>
2021-03-25 12:46:20 +0100

    created InvoiceManagerService

--- fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchCustomerService.java
+++ fakturogen-comarch-connector/src/main/java/pl/fakturogen/comarch/connector/services/ComarchCustomerService.java
@@ -1,9 +1,11 @@
 package pl.fakturogen.comarch.connector.services;
 
+import org.springframework.stereotype.Service;
 import pl.fakturogen.invoice.web.dto.CustomerDTO;
 
 import java.util.Optional;
 
+@Service
 public interface ComarchCustomerService {
 
     Optional<CustomerDTO> read(long id);
--- fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/InvoiceManagerService.java
+++ fakturogen-invoice/src/main/java/pl/fakturogen/invoice/service/InvoiceManagerService.java
@@ -1,10 +1,19 @@
 package pl.fakturogen.invoice.service;
 
 import org.springframework.stereotype.Service;
+import pl.fakturogen.comarch.connector.dto.ComarchInvoiceDTO;
+import pl.fakturogen.comarch.connector.services.ComarchCustomerService;
+import pl.fakturogen.comarch.connector.services.ComarchInvoiceService;
+import pl.fakturogen.invoice.service.mapper.InvoiceMapper;
+import pl.fakturogen.invoice.web.dto.CustomerDTO;
+import pl.fakturogen.invoice.web.dto.InvoiceDTO;
+import pl.fakturogen.invoice.web.dto.InvoiceSaveDTO;
+
+import java.util.Optional;
 
 @Service
 public class InvoiceManagerService {
-    /*private InvoiceMapper invoiceMapper;
+    private InvoiceMapper invoiceMapper;
     private ComarchInvoiceService comarchInvoiceService;
     private ComarchCustomerService comarchCustomerService;
     private CustomerService customerService;
@@ -25,12 +34,12 @@ public class InvoiceManagerService {
         Integer purchasingPartyId = comarchInvoiceDTO.getPurchasingPartyId();
         Optional<CustomerDTO> customerDTO = customerService.findByExternalId(purchasingPartyId);
 
-        if(customerDTO.isPresent()){
+        if(customerDTO.isPresent()){ //.orElseThrow
             invoiceToSave.setCustomer(customerDTO.get());
             return invoiceService.create(invoiceToSave);
         }
         Optional<CustomerDTO> comarchCustomer = comarchCustomerService.read(purchasingPartyId);
-        comarchCustomer.ifPresent(invoiceToSave::setCustomer);
+        comarchCustomer.ifPresent(invoiceToSave::setCustomer);// orElseThrow
         return invoiceService.create(invoiceToSave);
-    }*/
+    }
 }
