523ec5fb476ede689faa62e7857aa856c64dee3d
KrzKos <krzysztof.kostkiewicz@gmail.com>
2021-03-25 22:56:16 +0100

    created context for invoice repository test

---
+++ fakturogen-invoice/src/test/java/pl/fakturogen/invoice/config/InvoiceTestContextConfiguration.java
@@ -0,0 +1,64 @@
+package pl.fakturogen.invoice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = {"pl.fakturogen.invoice"})
+@EnableJpaRepositories(basePackages = {"pl.fakturogen.invoice.dao"})
+public class InvoiceTestContextConfiguration {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("pl.fakturogen.invoice.dao");
+        emf.setPersistenceUnitName("invoicePU");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setJpaProperties(jpaProperties());
+
+        return emf;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/h2database/fakturogen-test;DB_CLOSE_ON_EXIT=FALSE;DB_CLOSE_DELAY=-1;AUTO_SERVER=TRUE;FILE_LOCK=SOCKET;");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        return properties;
+    }
+}
