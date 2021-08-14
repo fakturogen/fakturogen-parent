932d84611c2f7e6fc23f26fba7ba4fb64e213a4b
rowinskidamian <damian.rowinski@gmail.com>
2021-08-14 12:45:03 +0200

    added test find by productId

--- fakturogen-invoice/src/test/java/pl/fakturogen/invoice/dao/repository/ProductRepositoryIntegrationTest.java
+++ fakturogen-invoice/src/test/java/pl/fakturogen/invoice/dao/repository/ProductRepositoryIntegrationTest.java
@@ -1,5 +1,6 @@
 package pl.fakturogen.invoice.dao.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import pl.fakturogen.invoice.dao.entity.Product;
 import pl.fakturogen.invoice.dao.entity.Rate;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,24 +21,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductRepositoryIntegrationTest {
 
     @Autowired
+    public ProductRepositoryIntegrationTest(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     ProductRepository productRepository;
+    Product productInit;
+
+    @BeforeEach
+    public void init() {
+        productInit = new Product();
+        productInit.setName("Product name");
+        productInit.setDescription("Product description");
+        productInit.setSaleNetPrice(100.0);
+        productInit.setSaleGrossPrice(100.0);
+        productInit.setRate(Rate.R1);
+        productInit.setIdExternalApi(123L);
+    }
 
     @Test
     public void givenEntityWhenSaveShouldReturnIncreasedSizeByOne() {
-        Product product = new Product();
-        product.setName("Product name");
-        product.setDescription("Product description");
-        product.setSaleNetPrice(100.0);
-        product.setSaleGrossPrice(100.0);
-        product.setRate(Rate.R1);
-        product.setIdExternalApi(123L);
-
-
         List<Product> productList = productRepository.findAll();
         int currentProductListSize = productList.size();
         int expectedProductListSize = currentProductListSize + 1;
 
-        Product savedProduct = productRepository.save(product);
+        Product savedProduct = productRepository.save(productInit);
         List<Product> listAfterExtension = productRepository.findAll();
         int currentListSize = listAfterExtension.size();
 
@@ -47,5 +56,32 @@ class ProductRepositoryIntegrationTest {
                         + expectedProductListSize)
         );
     }
+
+    @Test
+    public void givenProductIdShouldFindProduct() {
+        Product productExpected = new Product();
+        productExpected.setName("Product name");
+        productExpected.setDescription("Product description");
+        productExpected.setSaleNetPrice(100.0);
+        productExpected.setSaleGrossPrice(100.0);
+        productExpected.setRate(Rate.R1);
+        productExpected.setIdExternalApi(123L);
+
+        Product savedProduct = productRepository.save(productInit);
+        Long productId = savedProduct.getId();
+        productExpected.setId(productId);
+
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        Product productFound = optionalProduct.orElseThrow();
+
+        assertAll(() -> assertEquals(productExpected.getId(), productFound.getId()),
+                () -> assertEquals(productExpected.getName(), productFound.getName()),
+                () -> assertEquals(productExpected.getDescription(), productFound.getDescription()),
+                () -> assertEquals(productExpected.getSaleNetPrice(), productFound.getSaleNetPrice()),
+                () -> assertEquals(productExpected.getSaleGrossPrice(), productFound.getSaleGrossPrice()),
+                () -> assertEquals(productExpected.getRate(), productFound.getRate()),
+                () -> assertEquals(productExpected.getIdExternalApi(), productFound.getIdExternalApi()));
+    }
+
 
 }
