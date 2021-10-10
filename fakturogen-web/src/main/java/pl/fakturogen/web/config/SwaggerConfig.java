19991739be7ad9f5c54550595b37510d3b92c28a
rowinskidamian <damian.rowinski@gmail.com>
2021-10-10 11:02:41 +0200

    added api info

--- fakturogen-web/src/main/java/pl/fakturogen/web/config/SwaggerConfig.java
+++ fakturogen-web/src/main/java/pl/fakturogen/web/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package pl.fakturogen.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +14,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Value("${fakturogen.application.version}")
+    private String appVersion;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
                 .apis(RequestHandlerSelectors.basePackage("pl.fakturogen.web.controller"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Fakturogen - invoices automatization")
+                .version(appVersion)
+                .description("App gives you possibility to connect to external invoice application, get your invoices, "
+                        + "automatically generates invoices propositions (templates) for next month, and "
+                        + "upload batch of new similar invoices for new month. It will make easier life for people "
+                        + "who has multiple similar invoices to be created every month.")
                 .build();
     }
 
